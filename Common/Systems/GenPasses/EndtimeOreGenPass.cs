22f3f6556033375e23e5c4278326e27ff469c62e
Axurliz <axurlizmail@gmail.com>
2022-05-26 14:53:46 +0200

    Make Electrum spawn in Underground Desert only

--- Common/Systems/GenPasses/EndtimeOreGenPass.cs
+++ Common/Systems/GenPasses/EndtimeOreGenPass.cs
@@ -15,50 +15,23 @@ namespace Endtime.Common.Systems.GenPasses
         {
             progress.Message = "Spawning End Time Ores";
 
-            for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++) {
+            for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 1E-3); k++) {
                 int x = WorldGen.genRand.Next(0, Main.maxTilesX);
                 int y = WorldGen.genRand.Next((int)WorldGen.worldSurfaceLow, Main.maxTilesY);
 
-                    WorldGen.TileRunner(x, y, WorldGen.genRand.Next(5, 8), WorldGen.genRand.Next(3, 6), ModContent.TileType<ElectrumOre>());
+                Tile tile = Framing.GetTileSafely(x, y);
+				if (tile.TileType == TileID.Sandstone || tile.TileType == TileID.DesertFossil)
+				{
+					WorldGen.TileRunner(x, y, WorldGen.genRand.Next(5, 8), WorldGen.genRand.Next(3, 6), ModContent.TileType<ElectrumOre>());
+				}
             }
 
             for (int k = 0; k < (int)(Main.maxTilesX * Main.maxTilesY * 6E-05); k++) {
                 int x = WorldGen.genRand.Next(0, Main.maxTilesX);
-                int y = WorldGen.genRand.Next((int)WorldGen.worldSurfaceLow, Main.maxTilesY);
+                int y = WorldGen.genRand.Next((int)WorldGen.rockLayer, Main.maxTilesY);
 
                     WorldGen.TileRunner(x, y, WorldGen.genRand.Next(5, 8), WorldGen.genRand.Next(3, 6), ModContent.TileType<InfernalOre>());
             }
-
-            // int maxToSpawn = (int)(Main.maxTilesX * Main.maxTilesY * 6E-05);
-            // for(int i = 0; i < maxToSpawn; i++)
-            // {
-            //     int x = WorldGen.genRand.Next(0, Main.maxTilesX);
-            //     int y = WorldGen.genRand.Next((int)WorldGen.worldSurface, Main.maxTilesY);
-
-            //     WorldGen.TileRunner(x, y, WorldGen.genRand.Next(5, 8), WorldGen.genRand.Next(3, 6), ModContent.TileType<ElectrumOre>());
-            // }
-
-            // maxToSpawn = WorldGen.genRand.Next(100, 250);
-            // int numSpawned = 0;
-            // int attempts = 0;
-            // while(numSpawned < maxToSpawn)
-            // {
-            //     int x = WorldGen.genRand.Next(0, Main.maxTilesX);
-            //     int y = WorldGen.genRand.Next((int)WorldGen.rockLayerLow, Main.maxTilesY);
-
-            //     Tile tile = Framing.GetTileSafely(x, y);
-            //     if(tile.TileType == TileID.Ash || tile.TileType == TileID.Hellstone)
-            //     {
-            //         WorldGen.TileRunner(x, y, WorldGen.genRand.Next(2, 5), WorldGen.genRand.Next(1, 4), ModContent.TileType<FierymOre>());
-            //         numSpawned++;
-            //     }
-
-            //     attempts++;
-            //     if(attempts >= 100000)
-            //     {
-            //         break;
-            //     }
-            // }
         }
     }
 }
